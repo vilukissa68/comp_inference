45bdfc6aa73c2b46ce1a5077b8b65a21c93ef8db
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2025-10-20 13:19:03 +0300

    Fix module naming

--- comp_inference/__init__.py
+++ comp_inference/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ._core import replace_linear_with_compressed
+from .comp_infernece import replace_linear_with_compressed
 from .layers.compressed_linear import CompressedLinear
 
 __all__ = ["replace_linear_with_compressed", "CompressedLinear"]
